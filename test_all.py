52fdc493d153ca2c2397550018378bd936e1d079
D.V. Nguyen <dvnguyen.vn@gmail.com>
2017-05-30 20:32:02 -0500

    Refactor tests to TestCase's

--- test_all.py
+++ test_all.py
@@ -1,6 +1,7 @@
 from postgrespy.models import Model
 from postgrespy.fields import TextField, IntegerField, BooleanField, JsonBField
 from postgrespy.queries import Select, Join
+from unittest import TestCase
 
 
 class Student(Model):
@@ -29,117 +30,148 @@ class Car(Model):
         table = 'cars'
 
 
-def test_save_load_delete():
-    peter = Student(name='Peter', age=15)
-    peter.save()
-    assert(type(peter.age) == IntegerField)
-    assert peter.age == 15
+class SaveLoadDeleteTestCase(TestCase):
+    def setUp(self):
+        self.peter = Student(name='Peter', age=15)
+        self.peter.save()
+        self.still_peter = Student(id=self.peter.id)
+        # self.no_one = Student.fetchone(id=999, name='hehe')
 
-    still_peter = Student(id=peter.id)
-    assert(type(still_peter.age) == IntegerField)
-    still_peter.age = 16
-    still_peter.save()
-    assert still_peter.name == 'Peter'
-    assert still_peter.age == 16
+        self.another_peter = Student(name='Peter', age=17)
+        self.another_peter.save()
 
-    another_peter = Student(name='Peter', age=17)
-    another_peter.save()
+    def test_save_load_delete(self):
 
-    with Select(Student, 'name=%s') as select:
-        select.execute(('Peter', ))
-        two_peters = select.fetchall()
-        assert len(two_peters) == 2
+        assert(type(self.peter.age) == IntegerField)
+        assert self.peter.age == 15
 
-    peter.delete()
-    still_peter.delete()
-    another_peter.delete()
+        assert(type(self.still_peter.age) == IntegerField)
+        self.still_peter.age = 16
+        self.still_peter.save()
+        assert self.still_peter.name == 'Peter'
+        assert self.still_peter.age == 16
 
-    with Select(Student, 'name=%s') as select:
-        select.execute(('Peter', ))
-        none_peters = select.fetchall()
-        assert len(none_peters) == 0
+        with Select(Student, 'name=%s') as select:
+            select.execute(('Peter', ))
+            two_peters = select.fetchall()
+            assert len(two_peters) == 2
 
-
-def test_boolean_field():
-    transgender = Student(name='HG', age=27, is_male=True)
-    transgender.save()
-    assert transgender.is_male == True
-
-    transgender.is_male = False
-    transgender.save()
-
-    still_trangender = Student(id=transgender.id)
-    assert still_trangender.is_male == False
-
-    transgender.delete()
-    still_trangender.delete()
+    def tearDown(self):
+        self.peter.delete()
+        self.still_peter.delete()
+        self.another_peter.delete()
 
 
-def test_jsonb_field():
-    tom = Student(name='Tom')
-    tom.save()
-    meth = Product(name='meth', owner_id=tom.id, detail={
-        'color': 'red',
-        'weight': 5
-    })
-    meth.save()
+class BooleanTestCase(TestCase):
+    def setUp(self):
+        self.transgender = Student(name='HG', age=27, is_male=True)
+        self.transgender.save()
 
-    assert(meth.detail['color'] == 'red')
+    def test_boolean_field(self):
 
-    tom.delete()
+        assert self.transgender.is_male == True
 
+        self.transgender.is_male = False
+        self.transgender.save()
 
-def test_get_all():
-    phil = Student(name='Phil', age=27)
-    phil.save()
-    thor = Student(name='Thor', age=33)
-    thor.save()
+        still_trangender = Student(id=self.transgender.id)
+        assert still_trangender.is_male == False
 
-    all_adults = Student.getall()
-    assert len(all_adults) == 2
-    phil.delete()
-    thor.delete()
+        still_trangender.delete()
+
+    def tearDown(self):
+        self.transgender.delete()
 
 
-def test_joins():
-    tom = Student(name='Tom', age=20)
-    jerry = Student(name='Jerry', age=20)
-    bob = Student(name='Bob', age=20)
-    tom.save()
-    jerry.save()
-    bob.save()
+class JsonBTestCase(TestCase):
+    def setUp(self):
+        self.tom = Student(name='Tom')
+        self.tom.save()
+        self.meth = Product(name='meth', owner_id=self.tom.id, detail={
+            'color': 'red',
+            'weight': 5
+        })
+        self.meth.save()
 
-    weed = Product(name='weed', owner_id=tom.id, detail={})
-    smoke = Product(name='smoke', owner_id=tom.id, detail={})
-    book = Product(name='book', owner_id=jerry.id, detail={})
+    def test_jsonb_field(self):
+        assert(self.meth.detail['color'] == 'red')
 
-    weed.save()
-    smoke.save()
-    book.save()
+    def tearDown(self):
+        self.tom.delete()
 
-    toyota = Car(name='Toyota', owner_id=tom.id)
-    nissan = Car(name='Nissan', owner_id=tom.id)
-    toyota.save()
-    nissan.save()
 
-    with Join(Student, 'INNER JOIN', Product, 'students.id = products.owner_id') as join:
-        join.execute()
-        ret = join.fetchall()
+class GetAllTestCase(TestCase):
+    def setUp(self):
+        self.phil = Student(name='Phil', age=27)
+        self.phil.save()
+        self.thor = Student(name='Thor', age=33)
+        self.thor.save()
 
-    assert len(ret) == 3
+    def test_get_all(self):
+        all_adults = Student.getall()
+        assert len(all_adults) == 2
 
-    still_tom, still_smoke = ret[1]
-    assert still_tom.name == 'Tom'
-    assert still_smoke.name == 'smoke'
+    def tearDown(self):
+        self.phil.delete()
+        self.thor.delete()
 
-    with Join(Student, 'INNER JOIN', Product, 'students.id = products.owner_id',
-                       'INNER JOIN', Car, 'students.id = cars.owner_id') as join:
-        join.execute()
-        ret = join.fetchall()
-    assert len(ret) == 4
 
-    tom.delete()
-    jerry.delete()
-    bob.delete()
+def JoinTestCase(TestCase):
+    def setUp(self):
+        self.tom = Student(name='Tom', age=20)
+        self.jerry = Student(name='Jerry', age=20)
+        self.bob = Student(name='Bob', age=20)
+        self.tom.save()
+        self.jerry.save()
+        self.bob.save()
 
-    assert len(Product.getall()) == 0
+        self.weed = Product(name='weed', owner_id=self.tom.id, detail={})
+        self.smoke = Product(name='smoke', owner_id=self.tom.id, detail={})
+        self.book = Product(name='book', owner_id=self.jerry.id, detail={})
+
+        self.weed.save()
+        self.smoke.save()
+        self.book.save()
+
+        self.toyota = Car(name='Toyota', owner_id=self.tom.id)
+        self.nissan = Car(name='Nissan', owner_id=self.tom.id)
+        self.toyota.save()
+        self.nissan.save()
+
+    def test_joins(self):
+
+        with Join(Student, 'INNER JOIN', Product, 'students.id = products.owner_id') as join:
+            join.execute()
+            ret = join.fetchall()
+
+        assert len(ret) == 3
+
+        still_tom, still_smoke = ret[1]
+        assert still_tom.name == 'Tom'
+        assert still_smoke.name == 'smoke'
+
+        with Join(Student, 'INNER JOIN', Product, 'students.id = products.owner_id',
+                  'INNER JOIN', Car, 'students.id = cars.owner_id') as join:
+            join.execute()
+            ret = join.fetchall()
+        assert len(ret) == 4
+
+    def tearDown(self):
+        self.tom.delete()
+        self.jerry.delete()
+        self.bob.delete()
+
+
+class NothingTestCase(TestCase):
+    """This test case ensures that all objects have been deleted"""
+
+    def setUp(self):
+        pass
+
+    def test_nothing(self):
+        assert (len(Student.getall()) == 0)
+        assert (len(Product.getall()) == 0)
+        assert (len(Car.getall()) == 0)
+
+    def tearDown(self):
+        pass
