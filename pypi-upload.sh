6a5758b2785e0456d5c6e726a0885b3d8b5870f5
D.V. Nguyen <dvnguyen.vn@gmail.com>
2017-07-29 18:28:20 -0500

    Upload to pypi

---
+++ pypi-upload.sh
@@ -0,0 +1,2 @@
+python setup.py bdist_wheel
+twine upload dist/postgrespy-0.1.0-py3-none-any.whl
--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup (
   name = 'postgrespy',
   packages = ['postgrespy'],
-  version = '0.0.2',
+  version = '0.1.0',
   description = 'A simple postgres orm',
   author = 'Dung (DV) Nguyen',
   author_email = 'dv@dvnguyen.com',
   url = 'https://github.com/nguyendv/postgrespy',
-  download_url = 'https://github.com/nguyendv/postgrespy/archive/0.0.2.tar.gz',
+  download_url = 'https://github.com/nguyendv/postgrespy/archive/0.1.0.tar.gz',
   install_requires=[
     'psycopg2>=2.7',
     'Jinja2>=2.9'
