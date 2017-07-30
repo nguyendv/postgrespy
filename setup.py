1653a8ff8b3bcf9fdcc36d1f0e1995b8d0827df4
D.V. Nguyen <dvnguyen.vn@gmail.com>
2017-07-30 15:13:03 -0500

    Upload to pypi

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup
 
-from distutils.core import setup
-setup (
-  name = 'postgrespy',
-  packages = ['postgrespy'],
-  version = '0.1.0',
-  description = 'A simple postgres orm',
-  author = 'Dung (DV) Nguyen',
-  author_email = 'dv@dvnguyen.com',
-  url = 'https://github.com/nguyendv/postgrespy',
-  download_url = 'https://github.com/nguyendv/postgrespy/archive/0.1.0.tar.gz',
-  install_requires=[
-    'psycopg2>=2.7',
-    'Jinja2>=2.9'
-  ],
-  python_requires = '~=3.6',
-  keywords = [],
-  classifiers = []
+setup(
+    name='postgrespy',
+    packages=['postgrespy'],
+    version='0.2.0',
+    description='A simple postgres orm',
+    author='Dung (DV) Nguyen',
+    author_email='dv@dvnguyen.com',
+    url='https://github.com/nguyendv/postgrespy',
+    download_url='https://github.com/nguyendv/postgrespy/archive/0.2.0.tar.gz',
+    install_requires=[
+        'psycopg2>=2.7',
+        'Jinja2>=2.9'
+    ],
+    python_requires='~=3.6',
+    keywords=[],
+    classifiers=[]
 )
